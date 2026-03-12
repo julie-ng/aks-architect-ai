a1059887a04f0e844e49aaff4eee2a820cca71f7
Julie Ng <me@julie.io>
2026-03-12 14:32:13 +0100

    ui: use Inter as default font

--- advisor-ui/nuxt.config.ts
+++ advisor-ui/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  modules: ['@nuxt/ui', '@nuxtjs/mdc', '@nuxt/eslint'],
+  modules: ['@nuxt/ui', '@nuxtjs/mdc', '@nuxt/eslint', '@nuxt/fonts'],
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
   runtimeConfig: {
