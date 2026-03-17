16f162f41ef88d34906002a68714cd424241caa5
Julie Ng <me@julie.io>
2026-03-17 10:38:42 +0100

    advisor-api: move config to environment variables

--- advisor-api/app/config.py
+++ advisor-api/app/config.py
@@ -1,9 +1,15 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings
 
 
+# Default Configuration
+# - overwritten by environment variables. See docs:
+# - https://docs.pydantic.dev/latest/concepts/pydantic_settings/#parsing-environment-variable-values
 class Settings(BaseSettings):
+    app_environment: Literal["development", "production"] = "production"
     qdrant_url: str = "http://localhost:6333"
-    qdrant_collection: str = "aks-docs"
+    qdrant_collection: str = "collection"
     embedding_model: str = "nomic-embed-text"
     embedding_prefix: str = "search_query: "
     document_prefix: str = "search_document: "
